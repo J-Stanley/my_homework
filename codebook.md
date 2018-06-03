ed4992b9e00284a8b96a7f70cacd22c5510a8c8a
J-Stanley <J-Stanley@users.noreply.github.com>
2018-06-03 20:32:38 +0800

    homework
    homework

---
+++ codebook.md
@@ -0,0 +1,5 @@
+1. activities_file and features_file extract the activity and feature names from the file, activities and features and activitynames extract the names as characters. 
+2. xtrain xtest ytrain ytest extract the data of the train and test data and the labels of the subject. then the two data sets are merged as x.     
+3. mean and sd is the mand and standard deviation of each varaible.
+4.newx is a data set, which is described by proper activity names.
+5. newx2, newx3, are the data set created for the final tidy data set newx4(tidy set), which is the data set of mean and standard deviation of each activity and subject.
--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-activities_file <- read.table("./UCI HAR Dataset/activity_labels.txt")                              ## 1
+activities_file <- read.table("./UCI HAR Dataset/activity_labels.txt")                              ## step 1
 activities <- as.character(activities_file$V2)
 features_file <- read.table("./UCI HAR Dataset/features.txt")
 features <- as.character(features_file$V2)
@@ -15,7 +15,7 @@ subjecttrain <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 subjecttest <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 subjectnames_file <- rbind(subjecttrain, subjecttest)
 subjects <- subjectnames_file$V1                                               
-mean <- sapply(x[,1:561], mean)                                                                     ## 2
+mean <- sapply(x[,1:561], mean)                                                                     ## step 2
 sd <- sapply(x[,1:561], sd)
 meanAndsd <- rbind(mean, sd)
 for(i in 1:6){
@@ -24,9 +24,9 @@ for(i in 1:6){
             activitynames[j] <- activities[i]
     }
 }  
-newx <- cbind(activitynames, x)                                                                     ## 3 
-newx2 <- cbind(subjects, x)                                                                         ## 4
-newx3 <- cbind(activitynames, newx2)                                                                ## 5
+newx <- cbind(activitynames, x)                                                                     ## step 3
+newx2 <- cbind(subjects, x)                                                                         ## step 4
+newx3 <- cbind(activitynames, newx2)                                                                ## step 5
 newx4 <- data.frame()
 for(i in 1:6){
     for(j in 1:30){
